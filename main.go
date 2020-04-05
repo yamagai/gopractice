f61185ffe847029f9924a240bef07fcea4880ac9
山本浩平 <>
2020-04-05 14:06:07 +0900

    基本セット準備

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "log"
+    "net/http"
+)
+
+func main() {
+    //ディレクトリを指定する
+    fs := http.FileServer(http.Dir("static"))
+    //ルーティング設定。"/"というアクセスがきたらstaticディレクトリのコンテンツを表示させる
+    http.Handle("/", fs)
+
+    log.Println("Listening...")
+    // 3000ポートでサーバーを立ち上げる
+    http.ListenAndServe(":3000", nil)
+}
