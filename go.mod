0855a13fbaedea87256a1a03986d633ce51b7782
山本浩平 <>
2020-04-15 15:51:10 +0900

    Revert "SQLboiler導入,ToDo途中"
    This reverts commit 66445dd190848110ea557bb83c1950448df703a9.

--- go.mod
+++ go.mod
@@ -2,22 +2,4 @@ module go_practice
 
 go 1.14
 
-require (
-	github.com/friendsofgo/errors v0.9.2 // indirect
-	github.com/fsnotify/fsnotify v1.4.9 // indirect
-	github.com/gin-gonic/gin v1.6.2 // indirect
-	github.com/go-sql-driver/mysql v1.5.0 // indirect
-	github.com/mitchellh/mapstructure v1.2.2 // indirect
-	github.com/pelletier/go-toml v1.7.0 // indirect
-	github.com/spf13/afero v1.2.2 // indirect
-	github.com/spf13/cast v1.3.1 // indirect
-	github.com/spf13/cobra v1.0.0 // indirect
-	github.com/spf13/jwalterweatherman v1.1.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.6.3 // indirect
-	github.com/volatiletech/inflect v0.0.0-20170731032912-e7201282ae8d // indirect
-	github.com/volatiletech/sqlboiler v3.7.0+incompatible // indirect
-	golang.org/x/sys v0.0.0-20200413165638-669c56c373c4 // indirect
-	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
-	gopkg.in/ini.v1 v1.55.0 // indirect
-)
+require github.com/gin-gonic/gin v1.6.2 // indirect
--- main.go
+++ main.go
@@ -1,91 +1,37 @@
 package main
 
 import (
-  "fmt"
   "time"
   "net/http"
   "github.com/gin-gonic/gin"
-  _ "github.com/go-sql-driver/mysql"
-  . "github.com/volatiletech/sqlboiler/queries/qm"
-  "github.com/volatiletech/sqlboiler/boil"
-
 )
 
 type ToDo struct {
-  ID int `form:"id"`
-  Hito string `form:"hito"`
-  Content string `form:"content"`
-  Status int `form:"status"`
+  Hito string
+  Content string
+  Status int
   CreatedAt time.Time
-  CreatedAtS string
 }
 
-var todo []ToDo
-var idMax = 0
-
-func Saiban() int{
-  idMax = idMax + 1
-  return idMax
-}
-func GetDataToDo(c *gin.Context) {
-    var b ToDo
-    c.Bind(&b)
-    b.ID = Saiban()
-    b.Status = 0
-    b.CreatedAtS = time.Now().Format("2006-01-02 15:04:05")
-    todo = append(todo, b)
-    c.HTML(http.StatusOK, "index.html", map[string]interface{}{
-      "todo": todo,
-    })
-}
-func GetDoneToDo(c *gin.Context) {
-  var b ToDo
-  if err := c.Bind(&b); err != nil {
-    fmt.Errorf("%#v", err)
-  }
-  var s int
-
-  if b.Status == 0 {
-    s = 1
-  } else {
-    s = 0
-  }
-
-  for idx, t := range todo {
-    if t.ID == b.ID {
-      todo[idx].Status = s
-    }
-  }
-
-    c.HTML(http.StatusOK, "index.html", map[string]interface{}{
-      "todo": todo,
-    })
-}
 func main() {
-  todo = []ToDo{
+  todo := []ToDo{
     ToDo {
-      ID: Saiban(),
       Hito: "yamamoto",
       Content: "早起き",
       Status: 0,
       CreatedAt: time.Now(),
-      CreatedAtS: time.Now().Format("2006-01-02 15:04:05"),
     },
     ToDo {
-      ID: Saiban(),
       Hito: "katsuhei",
       Content: "インターン応募",
-      Status: 1,
+      Status: 0,
       CreatedAt: time.Now(),
-      CreatedAtS: time.Now().Format("2006-01-02 15:04:05"),
     },
     ToDo {
-      ID: Saiban(),
       Hito: "kohei",
       Content: "案件とる",
       Status: 0,
       CreatedAt: time.Now(),
-      CreatedAtS: time.Now().Format("2006-01-02 15:04:05"),
     },
   }
 
@@ -103,7 +49,5 @@ func main() {
       "todo": todo,
     })
 	})
-  r.GET("/yaru",GetDataToDo)
-  r.GET("/done",GetDoneToDo)
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
