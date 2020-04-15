b83d38da6fd849656605f07624082dd56f887885
山本浩平 <>
2020-04-15 21:45:15 +0900

    SQLboilerやめてGorm導入、ToDoから暇人掲示板に

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module go_practice
 
 go 1.14
 
-require github.com/gin-gonic/gin v1.6.2 // indirect
+require (
+	github.com/gin-gonic/gin v1.6.2
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/jinzhu/gorm v1.9.12 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	golang.org/x/sys v0.0.0-20200413165638-669c56c373c4 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+)
--- main.go
+++ main.go
@@ -1,53 +1,167 @@
 package main
 
 import (
-  "time"
-  "net/http"
-  "github.com/gin-gonic/gin"
+  "strconv"
+	"github.com/gin-gonic/gin"
+  "github.com/jinzhu/gorm"
+  _ "github.com/go-sql-driver/mysql"
 )
 
-type ToDo struct {
-  Hito string
-  Content string
-  Status int
-  CreatedAt time.Time
+type Himajin struct {
+    gorm.Model
+    Name   string
+    Begintime string
+    FinishTime string
+    Todo string
 }
 
+//DBマイグレート
+func dbInit() {
+    db, err := gorm.Open("mysql", "root:@(localhost)/gopractice?charset=utf8&parseTime=True")
+    if err != nil {
+        panic("データベース失敗（dbInit）")
+    }
+    db.AutoMigrate(&Himajin{})
+    defer db.Close()
+}
+//DB追加
+func dbInsert(name string, begintime string, finishtime string, todo string) {
+    db, err := gorm.Open("mysql", "root:@(localhost)/gopractice?charset=utf8&parseTime=True")
+    if err != nil {
+        panic("データベース失敗（dbInsert)")
+    }
+    db.Create(&Himajin{Name: name, Begintime: begintime, FinishTime: finishtime, Todo: todo})
+    defer db.Close()
+}
+
+//DB更新
+func dbUpdate(id int, name string, begintime string, finishtime string, todo string) {
+    db, err := gorm.Open("mysql", "root:@(localhost)/gopractice?charset=utf8&parseTime=True")
+    if err != nil {
+        panic("データベース失敗（dbUpdate)")
+    }
+    var himajin Himajin
+    db.First(&himajin, id)
+    himajin.Name = name
+    himajin.Begintime = begintime
+    himajin.FinishTime = finishtime
+    himajin.Todo = todo
+    db.Save(&himajin)
+    db.Close()
+}
+
+//DB削除
+func dbDelete(id int) {
+    db, err := gorm.Open("mysql", "root:@(localhost)/gopractice?charset=utf8&parseTime=True")
+    if err != nil {
+        panic("データベース失敗（dbDelete)")
+    }
+    var himajin Himajin
+    db.First(&himajin, id)
+    db.Delete(&himajin)
+    db.Close()
+}
+
+//DB全取得
+func dbGetAll() []Himajin {
+    db, err := gorm.Open("mysql", "root:@(localhost)/gopractice?charset=utf8&parseTime=True")
+    if err != nil {
+        panic("データベース失敗(dbGetAll())")
+    }
+    var himajins []Himajin
+    db.Order("created_at desc").Find(&himajins)
+    db.Close()
+    return himajins
+}
+
+//DB一つ取得
+func dbGetOne(id int) Himajin {
+    db, err := gorm.Open("mysql", "root:@(localhost)/gopractice?charset=utf8&parseTime=True")
+    if err != nil {
+        panic("データベース失敗(dbGetOne())")
+    }
+    var himajin Himajin
+    db.First(&himajin, id)
+    db.Close()
+    return himajin
+}
+
+
+
+
+
 func main() {
-  todo := []ToDo{
-    ToDo {
-      Hito: "yamamoto",
-      Content: "早起き",
-      Status: 0,
-      CreatedAt: time.Now(),
-    },
-    ToDo {
-      Hito: "katsuhei",
-      Content: "インターン応募",
-      Status: 0,
-      CreatedAt: time.Now(),
-    },
-    ToDo {
-      Hito: "kohei",
-      Content: "案件とる",
-      Status: 0,
-      CreatedAt: time.Now(),
-    },
-  }
+    router := gin.Default()
+    router.LoadHTMLGlob("./templates/*.html")
 
+    dbInit()
 
-  r := gin.Default()
-  r.LoadHTMLFiles("./templates/index.html")
-
-	r.GET("/hello", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-  r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", map[string]interface{}{
-      "todo": todo,
+    //Index
+    router.GET("/", func(ctx *gin.Context) {
+        himajin := dbGetAll()
+        ctx.HTML(200, "index.html", gin.H{
+            "himajins": himajin,
+        })
     })
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+    //Create
+    router.POST("/new", func(ctx *gin.Context) {
+        name := ctx.PostForm("name")
+        begintime := ctx.PostForm("begintime")
+        finishtime := ctx.PostForm("finishtime")
+        todo := ctx.PostForm("todo")
+        dbInsert(name, begintime, finishtime, todo)
+        ctx.Redirect(302, "/")
+    })
+
+    //Detail
+    router.GET("/detail/:id", func(ctx *gin.Context) {
+        n := ctx.Param("id")
+        id, err := strconv.Atoi(n)
+        if err != nil {
+            panic(err)
+        }
+        himajin := dbGetOne(id)
+        ctx.HTML(200, "detail.html", gin.H{"himajins": himajin})
+    })
+
+    //Update
+    router.POST("/update/:id", func(ctx *gin.Context) {
+        n := ctx.Param("id")
+        id, err := strconv.Atoi(n)
+        if err != nil {
+            panic("ERROR")
+        }
+        name := ctx.PostForm("name")
+        begintime := ctx.PostForm("begintime")
+        finishtime := ctx.PostForm("finishtime")
+        todo := ctx.PostForm("todo")
+        dbUpdate(id, name, begintime, finishtime, todo)
+        ctx.Redirect(302, "/")
+    })
+
+    //削除確認
+    router.GET("/delete_check/:id", func(ctx *gin.Context) {
+        n := ctx.Param("id")
+        id, err := strconv.Atoi(n)
+        if err != nil {
+            panic("ERROR")
+        }
+        himajin := dbGetOne(id)
+        ctx.HTML(200, "delete.html", gin.H{"himajins": himajin})
+    })
+
+    //Delete
+    router.POST("/delete/:id", func(ctx *gin.Context) {
+        n := ctx.Param("id")
+        id, err := strconv.Atoi(n)
+        if err != nil {
+            panic("ERROR")
+        }
+        dbDelete(id)
+        ctx.Redirect(302, "/")
+
+    })
+
+    router.Run()
 }
